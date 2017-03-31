7edf6e571066ca468d01c043441d3a8f9d039366
Sebastien Guiriec <sebastien.guiriec@intel.com>
2017-03-31 19:48:12 +0200

    NUC: Update mixins for CoE audio HAL
    This patch is updating the mixins for CoE Audio HAL implentation.
    
    Signed-off-by: Sebastien Guiriec <sebastien.guiriec@intel.com>

---
+++ groups/audio/android_ia/AndroidBoard.mk
@@ -0,0 +1,2 @@
+# Nuc Target specific audio configuration files
+include device/intel/android_ia/common/audio/AndroidBoard.mk
--- groups/audio/android_ia/product.mk
+++ groups/audio/android_ia/product.mk
@@ -7,11 +7,8 @@ PRODUCT_PACKAGES_DEBUG += \
 # Extended Audio HALs
 PRODUCT_PACKAGES += \
     audio.r_submix.default \
-    audio.hdmi.android_ia \
     audio.primary.android_ia \
     audio.usb.default \
-    audio_policy.default.so
+    audio_hal_configurable \
+    audio_hal_nuc
 
-PRODUCT_COPY_FILES += \
-    device/intel/android_ia/common/audio/mixer_paths.xml:system/etc/mixer_paths.xml \
-    device/intel/android_ia/common/audio/audio_policy.conf:system/etc/audio_policy.conf
