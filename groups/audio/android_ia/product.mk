e13ab818c76f901e7b20c621d23ff0608d9427ea
Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
2017-05-02 18:07:05 -0500

    Make audio.primary.android_ia conditional if stub audio HAL is used
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>

--- groups/audio/android_ia/product.mk
+++ groups/audio/android_ia/product.mk
@@ -4,11 +4,14 @@ PRODUCT_PACKAGES_DEBUG += \
          tinyplay \
          tinycap
 
+ifneq ($(USE_STUB_AUDIO_HAL), 1)
+PRODUCT_PACKAGES += \
+    audio.primary.android_ia
+endif
+
 # Extended Audio HALs
 PRODUCT_PACKAGES += \
-    audio.primary.android_ia \
     audio.r_submix.default \
     audio.usb.default \
     audio_policy.default.so \
     audio_configuration_files
-
